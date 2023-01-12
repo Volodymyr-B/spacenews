0433465b4a4ad8bb42e8dbc5b6e51f4ff5ea1645
VolodymyrB <vladimir.babich87@gmail.com>
2023-01-12 23:17:38 +0200

    add dynamic pagination hook

---
+++ src/hooks/useDynamicPagination.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+export const useDynamicPagination = () => {
+  const [limit, setLimit] = useState(15);
+  const [changeLimit, setChangeLimit] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+    return () => {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (changeLimit) {
+      setLimit((prev) => prev + 15);
+    }
+  }, [changeLimit]);
+
+  const scrollHandler = (e: any) => {
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setChangeLimit(true);
+    }
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) >
+      100
+    ) {
+      setChangeLimit(false);
+    }
+  };
+
+  return limit;
+};
