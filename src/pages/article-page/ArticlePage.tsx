b2c9e45aaeb408dc30c83df77b614a8c66f8b6c2
VolodymyrB <vladimir.babich87@gmail.com>
2023-01-13 20:11:18 +0200

    connect router-dom with article-page and RTK-query

--- src/pages/article-page/ArticlePage.tsx
+++ src/pages/article-page/ArticlePage.tsx
@@ -1,10 +1,16 @@
 import { useEffect } from "react";
 import { useLazyGetArticleQuery } from "../../store/spaceflightnews/spaceflightnews-api";
+import { useNavigate, useParams } from "react-router-dom";
 import { Button, Container, Paper, Typography } from "@mui/material";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export const ArticlePage = () => {
   const [getArticle, { data, isLoading, isError }] = useLazyGetArticleQuery();
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const goBack = () => navigate(-1);
+  const redirect = () => navigate("/", { replace: true });
 
   // not enough data from backend, filling up the space
   const articleInfo = new Array(5)
@@ -13,7 +19,11 @@ export const ArticlePage = () => {
   //--------------------------------------------------------
 
   useEffect(() => {
-    getArticle(17865);
+    if (id) {
+      getArticle(Number(id));
+    } else {
+      redirect();
+    }
   }, []);
 
   return (
@@ -24,13 +34,17 @@ export const ArticlePage = () => {
         alt={data?.imageUrl}
       />
       <Container maxWidth="xl" className="-mt-16 absolute left-0 right-0">
-        <Paper elevation={4} className="p-6 mb-8 min-h-[400px]">
-          <Typography variant="h6" component="h3" className="text-center mb-5">
+        <Paper elevation={4} className="p-6 mb-8 min-h-[400px] text-center">
+          <Typography variant="h6" component="h3" className="mb-5">
             {data?.title}
           </Typography>
           <div className="flex flex-col gap-3">{articleInfo}</div>
+          <div className="flex justify-around">
+            <Typography variant="h6">{data?.newsSite}</Typography>
+            <Typography variant="h6">{data?.publishedAt.toString()}</Typography>
+          </div>
         </Paper>
-        <Button size="small" className="ml-10">
+        <Button onClick={goBack} size="small" className="ml-10">
           <KeyboardBackspaceIcon />
           Back to homepage
         </Button>
