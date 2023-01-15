17847cf95ec421be7ce9dac711944945b74bee80
VolodymyrB <vladimir.babich87@gmail.com>
2023-01-15 11:24:44 +0200

    add mock text for article page.

---
+++ src/constants/mockText.tsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material";
+
+export const mockText = new Array(4)
+  .fill("")
+  .map((_, index) => (
+    <Typography key={index}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum qui rem
+      vitae dolore culpa inventore molestias, optio, aspernatur facere corporis
+      fugit ab dolores architecto autem nostrum quaerat minus commodi labore
+      corrupti sed nisi reiciendis nihil ad quam? Optio dolores molestiae neque
+      veniam dolor porro commodi est maxime voluptatum voluptatem, odio cum
+      amet, quidem ducimus accusantium perspiciatis iure. Cumque illo blanditiis
+      ratione earum veniam ad, totam, dolores qui explicabo error, possimus
+      minus ex velit non accusamus sed? Quibusdam perferendis dolorum porro
+      doloremque neque! Dolores numquam aliquid ratione voluptatem eos ea quam.
+    </Typography>
+  ));
--- src/pages/article-page/ArticlePage.tsx
+++ src/pages/article-page/ArticlePage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useLazyGetArticleQuery } from "../../store/spaceflightnews/spaceflightnews-api";
 import { useNavigate, useParams } from "react-router-dom";
 
+import { mockText } from "../../constants/mockText"; // not enough data from backend, filling up the space
 import { ArticlePageSkeleton } from "./ArticlePageSkeleton";
 
 import { Button, Container, Paper, Typography } from "@mui/material";
@@ -9,17 +10,12 @@ import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 export const ArticlePage = () => {
   const [getArticle, { data, isLoading, isError }] = useLazyGetArticleQuery();
+
   const navigate = useNavigate();
   const { id } = useParams();
 
   const goBack = () => navigate(-1);
   const redirect = () => navigate("/", { replace: true });
-
-  // not enough data from backend, filling up the space
-  const articleInfo = new Array(5)
-    .fill("")
-    .map((_, index) => <Typography key={index}>{data?.summary}</Typography>);
-  //--------------------------------------------------------
 
   useEffect(() => {
     if (id) {
@@ -30,26 +26,29 @@ export const ArticlePage = () => {
   }, []);
 
   if (isLoading) return <ArticlePageSkeleton />;
+  if (isError) return <>sometging wrong</>;
+  if (!data) return null;
+
+  const { title, newsSite, imageUrl, publishedAt, summary } = data;
 
   return (
     <>
-      <img
-        className="h-56 w-full object-cover"
-        src={data?.imageUrl}
-        alt={data?.imageUrl}
-      />
+      <img className="h-56 w-full object-cover" src={imageUrl} alt={imageUrl} />
       <Container maxWidth="xl" className="-mt-16 absolute left-0 right-0">
         <Paper elevation={4} className="p-6 mb-8 min-h-[400px] text-center">
           <Typography variant="h6" component="h3" className="mb-5">
-            {data?.title}
+            {title}
           </Typography>
-          <div className="flex flex-col gap-3">{articleInfo}</div>
+          <div className="flex flex-col gap-3">
+            {summary}
+            {mockText}
+          </div>
           <div className="flex justify-around">
             <Typography variant="h6" color="text.secondary">
-              {data?.newsSite}
+              {newsSite}
             </Typography>
             <Typography variant="h6" color="text.secondary">
-              {data?.publishedAt.toString()}
+              {publishedAt.toString()}
             </Typography>
           </div>
         </Paper>
