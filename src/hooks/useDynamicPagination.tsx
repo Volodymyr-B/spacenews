8042f2df6fb41c5e6aaabf451dfc8cdbe045f26c
VolodymyrB <vladimir.babich87@gmail.com>
2023-01-16 18:14:13 +0200

    refactor

--- src/hooks/useDynamicPagination.tsx
+++ src/hooks/useDynamicPagination.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useDynamicPagination = () => {
   const [limit, setLimit] = useState(15);
